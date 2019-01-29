a5aa9b5c06c77da0c9a7373cb5123c850ef5933f
markus <markus.pichler@tugraz.at>
2019-01-29 11:07:51 +0100

    setup py

--- setup.py
+++ setup.py
@@ -5,24 +5,17 @@ __email__ = "markus.pichler@tugraz.at"
 __version__ = "0.1"
 __license__ = "MIT"
 
-from setuptools import setup, find_packages
-
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
+from setuptools import setup
 
 setup(
     name='ehyd_tools',
     version='0.1',
-    packages=find_packages(),
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.json', '*.', '*.png', '*.pdf', '*.csv', '*.tex','*.xslx'],
-    },
+    packages=['ehyd_tools'],
     url='https://github.com/MarkusPic/ehyd_tools',
     license='MIT',
-    author='pichler',
+    author='Markus Pichler',
     author_email='markus.pichler@tugraz.at',
     description='Diverse tools to export and analyse the >10a rain series from the ehyd.gv.at platform',
     scripts=['bin/ehyd_tools'],
-    install_requires=requirements,
+    install_requires=['numpy', 'pandas', 'matplotlib', 'requests'],
 )
