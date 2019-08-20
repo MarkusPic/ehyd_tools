79078f8e4fc763005bba863a96f70c6978725846
Markus <markus.pichler@tugraz.at>
2019-08-20 14:15:32 +0200

    new ehyd file format

--- ehyd_tools/in_out.py
+++ ehyd_tools/in_out.py
@@ -5,7 +5,6 @@ __license__ = "MIT"
 __version__ = "1.0.0"
 __maintainer__ = "David Camhy, Markus Pichler"
 
-
 import codecs
 import requests
 from io import BytesIO, TextIOWrapper, IOBase, StringIO
@@ -14,8 +13,20 @@ from os import path
 import pandas as pd
 from pandas.errors import ParserError
 
+from idf_analysis.sww_utils import guess_freq
+
 
 def csv_args(unix=False):
+    """
+    how to read and write csv file
+    differs between windows and unix (mac+linux)
+
+    Args:
+        unix (bool): if it is a unix computer
+
+    Returns:
+        dict: arguments to read and write csv files
+    """
     if unix:
         return dict(sep=',', decimal='.')
     else:
@@ -23,6 +34,15 @@ def csv_args(unix=False):
 
 
 def check_path(pth=None):
+    """
+    use the local directory if not path is give
+
+    Args:
+        pth (str): path to directory
+
+    Returns:
+        str: path to directory
+    """
     if pth == '':
         return pth
     elif pth is None:
@@ -38,19 +58,15 @@ def export_series(series, filename, export_path=None, save_as='csv', unix=False)
     export the series to a given format
     may be extended
 
-    :param export_path: path where file will be stored
-    :type export_path: str
+    Args:
+        series (pandas.Series):
+        filename (str): name of the file
+        export_path (str): path where file will be stored
+        save_as (str): export format
+        unix (bool): whether to use "," or ";" for the csv
 
-    :param filename: name of the file
-    :type filename: str
-
-    :type series: pd.Series
-
-    :param save_as: export format
-    :type save_as: str
-
-    :param unix: whether to use "," or ";" for the csv
-    :type unix: bool
+    Returns:
+        str: path to created file
     """
     fn = path.join(check_path(export_path), '{}.{}'.format(filename, save_as))
 
@@ -69,12 +85,14 @@ def export_series(series, filename, export_path=None, save_as='csv', unix=False)
 def import_series(filename, series_label='precipitation', index_label='datetime', unix=False):
     """
 
-    :param filename:
-    :param series_label:
-    :param index_label:
-    :param unix: whether to use "," or ";" for the csv
-    :type unix: bool
-    :return:
+    Args:
+        filename (str):
+        series_label (str):
+        index_label (str):
+        unix (bool): whether to use "," or ";" for the csv
+
+    Returns:
+        pandas.Series: rain series
     """
     if filename.endswith('csv'):
         try:
@@ -140,9 +158,13 @@ ehyd_stations = {100180: 'Tschagguns',
 
 def _get_file(id_):
     """
+    get the file of the series of the station
 
-    :param id_:
-    :return:
+    Args:
+        id_ (int): Gitterpunktnummer (id number) of the station
+
+    Returns:
+        TextIOWrapper | IOBase:
     """
     url = 'https://ehyd.gv.at/eHYD/MessstellenExtraData/nlv?id={id}&file=2'.format(id=id_)
     r = requests.get(url, allow_redirects=True)
@@ -154,58 +176,123 @@ def _get_file(id_):
         return csv_file
 
 
-def _parse(filepath_or_buffer, series_label='precipitation', index_label='datetime', with_meta=False):
+def _read_file(filepath_or_buffer):
     """
+    read the file
 
-    :param filepath_or_buffer:
-    :param series_label:
-    :param index_label:
-    :return:
+    Args:
+        filepath_or_buffer (IOBase | str): file of the series or path to the local file
+
+    Returns:
+        IOBase: file of the series
     """
     if isinstance(filepath_or_buffer, str):
-        csv_file = codecs.open(filepath_or_buffer, 'r', encoding='iso8859')
+        return codecs.open(filepath_or_buffer, 'r', encoding='iso8859')
     elif isinstance(filepath_or_buffer, IOBase):
-        csv_file = filepath_or_buffer
+        return filepath_or_buffer
     else:
         raise NotImplementedError()
 
-    if with_meta:
-        meta = []
-    for line in csv_file:
-        if line.startswith('Werte:'):
+
+def _split_file(file):
+    """
+    split the file in meta-data and series-data
+
+    Args:
+        file (IOBase): file of the series
+
+    Returns:
+        (list, list): meta-data and time-series-data
+    """
+    lines = file.readlines()
+    file.close()
+
+    items = {':': 2,
+             '.': 2,
+             ';': 1,
+             ',': 1}
+    i = 0
+    for i, line in enumerate(lines):
+        if all([line.count(s) == n for s, n in items.items()]):
             break
-        elif with_meta:
-            meta.append(line)
-    if with_meta:
-        meta = pd.Series(meta).str.replace('\n', '').str.split(';', expand=True).fillna('').apply(lambda x: x.str.strip())
-        # meta = ''.join(meta)
 
-    f = csv_file.read().replace(' ', '').replace(',', '.')
-    csv_file.close()
-    ts = pd.read_csv(StringIO(f), sep=';', header=None, index_col=0, squeeze=True, names=[series_label],
-                     na_values=['Lücke'], date_parser=lambda s: pd.to_datetime(s, format='%d.%m.%Y%H:%M:%S'))
-    ts = ts.rename_axis(index_label, axis='index')
-    ts = ts.resample('1T').ffill()
+    meta = lines[:i]
+    data = lines[i:]
+    return meta, data
 
+
+def parse_ehyd_file(filepath_or_buffer, series_label='precipitation', index_label='datetime'):
+    """
+
+    Args:
+        filepath_or_buffer (IOBase | str):
+        series_label (str):
+        index_label (str):
+
+    Returns:
+        (list, pandas.Series): meta-data and time-series
+    """
+    csv_file = _read_file(filepath_or_buffer)
+
+    # ___________________________
+    meta, data = _split_file(csv_file)
+
+    # ___________________________
+    ts = pd.read_csv(StringIO('\n'.join(data).replace(' ', '')), sep=';', decimal=',', index_col=0, na_values='Lücke',
+                     header=None, squeeze=True, names=[series_label],
+                     date_parser=lambda s: pd.to_datetime(s, format='%d.%m.%Y%H:%M:%S')
+                     )
+
+    # ___________________________
+    ts.index.name = index_label
+    # ts = ts.rename_axis(index_label, axis='index')
+    return meta, ts
+
+
+def _parse(filepath_or_buffer, series_label='precipitation', index_label='datetime', with_meta=False):
+    """
+
+    Args:
+        filepath_or_buffer (IOBase | str):
+        series_label (str):
+        index_label (str):
+        with_meta (bool): whether to return meta data or not
+
+    Returns:
+        pandas.Series | (pandas.Series, pandas.Series):
+    """
+    meta, ts = parse_ehyd_file(filepath_or_buffer, series_label=series_label, index_label=index_label)
+
+    freq = guess_freq(ts.index)
+    ts = ts.resample(freq).ffill()
+
+    # ___________________________
     if with_meta:
+        # meta = '\n'.join(meta)
+        meta = pd.Series(meta).str.replace('\n', '').str.split(';', expand=True).fillna('').apply(
+            lambda x: x.str.strip())
         return ts, meta
+
     else:
         return ts
 
 
 def get_station(id_):
     """
+    get the name of the station based on the id number
 
-    :param id_:
-    :return:
+    Args:
+        id_ (int): number
+
+    Returns:
+        str: name of the station
     """
     return ehyd_stations[id_]
 
 
 def get_all_stations():
     """
-
-    :return:
+    print all station names and ids
     """
     for id, location in ehyd_stations:
         print(id, ':', location)
@@ -213,15 +300,14 @@ def get_all_stations():
 
 def get_series(id_, with_meta=False):
     """
+    get the series from the ehyd platform
 
-    :param id_:
-    :type id_: int
+    Args:
+        id_ (int): number
+        with_meta (bool): whether to return meta data or not
 
-    :param with_meta: whether to return meta data or not
-    :type with_meta: bool
-
-    :return: data or data with the meta-data
-    :rtype:  pd.Series | list[pd.Series, str]
+    Returns:
+        pandas.Series | list[pandas.Series, str]: mata or data with the meta-data
     """
     if id_ in ehyd_stations:
         print('You choose the station: "{}" with the id: "{}".'.format(get_station(id_), id_))
@@ -230,9 +316,13 @@ def get_series(id_, with_meta=False):
 
 def get_station_meta(id_):
     """
+    get the station meta data
 
-    :param id_:
-    :return:
+    Args:
+        id_ (int):
+
+    Returns:
+        str:
     """
     url = 'https://ehyd.gv.at/eHYD/MessstellenExtraData/nlv?id={id}&file=1'.format(id=id_)
     r = requests.get(url, allow_redirects=True)
@@ -241,9 +331,11 @@ def get_station_meta(id_):
         file = TextIOWrapper(BytesIO(c), encoding='iso8859')
         return file.read()
 
+
 # if __name__ == '__main__':
-#     print(pd.Series(ehyd_stations).to_string())
-#     NOW = time.time()
-#     print('{:0.0f}'.format(time.time() - NOW))
-#     get_series(105445)
-#     print('{:0.0f}'.format(time.time() - NOW))
+    # print(pd.Series(ehyd_stations).to_string())
+    # import time
+    # NOW = time.time()
+    # print('{:0.0f}'.format(time.time() - NOW))
+    # print(get_series(105445))
+    # print('{:0.0f}'.format(time.time() - NOW))
