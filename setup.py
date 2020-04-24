8ed2a8e4dd4a4c6377f6ba9c791cf2185e57c4a1
Markus Pichler <markus.pichler@tugraz.at>
2020-04-24 15:09:48 +0200

    update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='ehyd-tools',
-    version='0.1dev8',
+    version='0.1dev9',
     packages=['ehyd_tools'],
     url='https://github.com/MarkusPic/ehyd_tools',
     license='MIT',
